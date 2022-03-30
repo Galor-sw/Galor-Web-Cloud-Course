8844260c15f4bb66468b63646c05944ad8ebeafc
galor <galor546@gmail.com>
2022-03-30 16:50:34 +0300

    ex3, layout3 functionality added

--- exercises/ex3/js/javascript.js
+++ exercises/ex3/js/javascript.js
@@ -8,13 +8,32 @@
 // formObj.appendChild(newObj);
 // // or
 // formObj.prepend(newObj);
+var counter = 0;
 
-function addRectangle() {
-  let newRectangle = document.createElement("div");
-  let randOpacity = Math.random();
-  console.log(randOpacity);
-  newRectangle.setAttribute("style", `opacity:${randOpacity};`);
-  let mainLO3 = document.getElementById("main-LO3");
-  mainLO3.appendChild(newRectangle);
-  console.log("addRectangle test");
+function doSome() {
+  let changeRectangle = document
+    .getElementById("main-LO3")
+    .getElementsByTagName("div")[2];
+  let rectangleWidth;
+
+  if (counter % 2 == 0) {
+    rectangleWidth = window.getComputedStyle(changeRectangle).width;
+    
+    changeRectangle.setAttribute("style", `width:100%`);
+  } else {
+    changeRectangle.setAttribute("style", { rectangleWidth });
+  }
+  counter++;
+}
+function addRectangles() {
+  let i = 0;
+  while (i < 23) {
+    let newRectangle = document.createElement("div");
+    let randOpacity = Math.random();
+
+    newRectangle.setAttribute("style", `opacity:${randOpacity};`);
+    let mainLO3 = document.getElementById("main-LO3");
+    mainLO3.appendChild(newRectangle);
+    i++;
+  }
 }
